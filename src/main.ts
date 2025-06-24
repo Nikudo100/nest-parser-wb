63c8df7d3967b76366feaa81924939828a1f9110
Nikudo100 <kunachillo@gmail.com>
2025-06-24 19:11:41 +0300

    1

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3003',
+    origin: process.env.NEXT_PUBLIC_SERVER_FRONT ?? 'http://localhost:3003',
   });
   await app.listen(process.env.PORT ?? 3004);
 }
